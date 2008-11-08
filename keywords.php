56446ad226be2f7dc7a5f73037f4e4398d536509
mitchoyoshitaka <mitchoyoshitaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-08 14:14:38 +0000

    split off keywords file
    git-svn-id: https://plugins.svn.wordpress.org/yet-another-related-posts-plugin/trunk@72825 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ keywords.php
@@ -0,0 +1,39 @@
+<?php
+
+$overusedwords = array( '', 'a', 'an', 'the', 'and', 'of', 'i', 'to', 'is', 'in', 'with', 'for', 'as', 'that', 'on', 'at', 'this', 'my', 'was', 'our', 'it', 'you', 'we', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '10', 'about', 'after', 'all', 'almost', 'along', 'also', 'amp', 'another', 'any', 'are', 'area', 'around', 'available', 'back', 'be', 'because', 'been', 'being', 'best', 'better', 'big', 'bit', 'both', 'but', 'by', 'c', 'came', 'can', 'capable', 'control', 'could', 'course', 'd', 'dan', 'day', 'decided', 'did', 'didn', 'different', 'div', 'do', 'doesn', 'don', 'down', 'drive', 'e', 'each', 'easily', 'easy', 'edition', 'end', 'enough', 'even', 'every', 'example', 'few', 'find', 'first', 'found', 'from', 'get', 'go', 'going', 'good', 'got', 'gt', 'had', 'hard', 'has', 'have', 'he', 'her', 'here', 'how', 'if', 'into', 'isn', 'just', 'know', 'last', 'left', 'li', 'like', 'little', 'll', 'long', 'look', 'lot', 'lt', 'm', 'made', 'make', 'many', 'mb', 'me', 'menu', 'might', 'mm', 'more', 'most', 'much', 'name', 'nbsp', 'need', 'new', 'no', 'not', 'now', 'number', 'off', 'old', 'one', 'only', 'or', 'original', 'other', 'out', 'over', 'part', 'place', 'point', 'pretty', 'probably', 'problem', 'put', 'quite', 'quot', 'r', 're', 'really', 'results', 'right', 's', 'same', 'saw', 'see', 'set', 'several', 'she', 'sherree', 'should', 'since', 'size', 'small', 'so', 'some', 'something', 'special', 'still', 'stuff', 'such', 'sure', 'system', 't', 'take', 'than', 'their', 'them', 'then', 'there', 'these', 'they', 'thing', 'things', 'think', 'those', 'though', 'through', 'time', 'today', 'together', 'too', 'took', 'two', 'up', 'us', 'use', 'used', 'using', 've', 'very', 'want', 'way', 'well', 'went', 'were', 'what', 'when', 'where', 'which', 'while', 'white', 'who', 'will', 'would', 'your');
+
+function yarpp_extract_keywords($source,$num_to_ret = 20) {
+	global $post, $overusedwords;
+	
+	if (function_exists('mb_split'))
+		$wordlist = mb_split('\s*[^\w]+\s*', strtolower($source));
+	else
+		$wordlist = split('\s*[^\w]+\s*', strtolower($source));	
+
+	// Build an array of the unique words and number of times they occur.
+	$a = array_count_values($wordlist);
+	
+	// Remove the stop words from the list.
+	foreach ($overusedwords as $word) {
+		 unset($a[$word]);
+	}
+	arsort($a, SORT_NUMERIC);
+	
+	$num_words = count($a);
+	$num_to_ret = $num_words > $num_to_ret ? $num_to_ret : $num_words;
+	
+	$outwords = array_slice($a, 0, $num_to_ret);
+	return implode(' ', array_keys($outwords));
+}
+
+function post_title_keywords($num_to_ret = 20) {
+	global $post;
+	return yarpp_extract_keywords($post->post_title);
+}
+
+function post_body_keywords($num_to_ret = 20) {
+	global $post;
+	return yarpp_extract_keywords($post->post_content);
+}
+
+?>
