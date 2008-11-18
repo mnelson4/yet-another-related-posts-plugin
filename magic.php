0baa12f6a63258210da2f71cba1e9e1107b3baea
mitchoyoshitaka <mitchoyoshitaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-11-18 11:53:04 +0000

    jic
    git-svn-id: https://plugins.svn.wordpress.org/yet-another-related-posts-plugin/trunk@74545 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- magic.php
+++ magic.php
@@ -41,7 +41,7 @@ function yarpp_sql($type,$args,$giveresults = true,$domain='website') {
 	// if cross_relate is set, override the type argument and make sure both matches are accepted in the sql query
 	if ($cross_relate) $type = array('post','page');
 
-	yarpp_cache_keywords(); /* TEST */
+	//yarpp_cache_keywords(); /* TEST */
 
 	// Fetch keywords
     $body_terms = post_body_keywords();
