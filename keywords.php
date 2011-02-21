02d0be09e288f578b6514dd1e3f9fa2b90d2d9af
mitchoyoshitaka <mitchoyoshitaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-02-21 19:22:24 +0000

    wp-typography on blacklist
    git-svn-id: https://plugins.svn.wordpress.org/yet-another-related-posts-plugin/trunk@348816 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- keywords.php
+++ keywords.php
@@ -52,3 +52,77 @@ function post_body_keywords($ID,$max = 20) {
 	$content = html_entity_strip($content);
 	return yarpp_extract_keywords($content,$max);
 }
+
+/* new in 2.0! apply_filters_if_white (previously apply_filters_without) now has a blacklist. It's defined here. */
+
+/* blacklisted so far:
+	- diggZ-Et
+	- reddZ-Et
+	- dzoneZ-Et
+	- WP-Syntax
+	- Viper's Video Quicktags
+	- WP-CodeBox
+	- WP shortcodes
+	- WP Greet Box
+	- wp-Typography @since 3.2.1
+	//- Tweet This - could not reproduce problem.
+*/
+
+$yarpp_blacklist = array(null,'yarpp_default','diggZEt_AddBut','reddZEt_AddBut','dzoneZEt_AddBut','wp_syntax_before_filter','wp_syntax_after_filter','wp_codebox_before_filter','wp_codebox_after_filter','do_shortcode');//,'insert_tweet_this'
+$yarpp_blackmethods = array(null,'addinlinejs','replacebbcode','filter_content','process');
+
+function yarpp_white($filter) {
+	global $yarpp_blacklist;
+	global $yarpp_blackmethods;
+	if (is_array($filter)) {
+		if (array_search($filter[1],$yarpp_blackmethods)) //print_r($filter[1]);
+			return false;
+	}
+	if (array_search($filter,$yarpp_blacklist)) //print_r($filter);
+		return false;
+	return true;
+}
+
+/* FYI, apply_filters_if_white was used here to avoid a loop in apply_filters('the_content') > yarpp_default() > yarpp_related() > current_post_keywords() > apply_filters('the_content').*/
+function apply_filters_if_white($tag, $value) {
+	global $wp_filter, $merged_filters, $wp_current_filter;
+
+	$args = array();
+	$wp_current_filter[] = $tag;
+
+	// Do 'all' actions first
+	if ( isset($wp_filter['all']) ) {
+		$args = func_get_args();
+		_wp_call_all_hook($args);
+	}
+
+	if ( !isset($wp_filter[$tag]) ) {
+		array_pop($wp_current_filter);
+		return $value;
+	}
+
+	// Sort
+	if ( !isset( $merged_filters[ $tag ] ) ) {
+		ksort($wp_filter[$tag]);
+		$merged_filters[ $tag ] = true;
+	}
+
+	reset( $wp_filter[ $tag ] );
+
+	if ( empty($args) )
+		$args = func_get_args();
+
+	do {
+		foreach( (array) current($wp_filter[$tag]) as $the_ )
+			if ( !is_null($the_['function'])
+			and yarpp_white($the_['function'])){ // HACK
+				$args[1] = $value;
+				$value = call_user_func_array($the_['function'], array_slice($args, 1, (int) $the_['accepted_args']));
+			}
+
+	} while ( next($wp_filter[$tag]) !== false );
+
+	array_pop( $wp_current_filter );
+
+	return $value;
+}
