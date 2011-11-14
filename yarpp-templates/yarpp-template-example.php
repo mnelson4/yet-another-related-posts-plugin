2bf37836cc8b61294f54a17181ce96ae7cb23ea6
mitchoyoshitaka <mitchoyoshitaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-11-14 01:29:53 +0000

    tagging 3.4b6
    git-svn-id: https://plugins.svn.wordpress.org/yet-another-related-posts-plugin/tags/3.4b6@462527 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- yarpp-templates/yarpp-template-example.php
+++ yarpp-templates/yarpp-template-example.php
@@ -3,9 +3,9 @@ Example template
 Author: mitcho (Michael Yoshitaka Erlewine)
 */
 ?><h3>Related Posts</h3>
-<?php if ($related_query->have_posts()):?>
+<?php if (have_posts()):?>
 <ol>
-	<?php while ($related_query->have_posts()) : $related_query->the_post(); ?>
+	<?php while (have_posts()) : the_post(); ?>
 	<li><a href="<?php the_permalink() ?>" rel="bookmark"><?php the_title(); ?></a><!-- (<?php the_score(); ?>)--></li>
 	<?php endwhile; ?>
 </ol>
