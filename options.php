9c2a956cabdd94519ca2b1a6b00869918d0ab27b
mitchoyoshitaka <mitchoyoshitaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-09-17 04:08:30 +0000

    tagging 3.5.3
    git-svn-id: https://plugins.svn.wordpress.org/yet-another-related-posts-plugin/tags/3.5.3@599889 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- options.php
+++ options.php
@@ -1,11 +1,8 @@
 <?php
 global $wpdb, $wp_version, $yarpp;
 
-// Reenforce YARPP setup:
-if ( !get_option('yarpp_version') )
-	$yarpp->activate();
-else
-	$yarpp->upgrade_check();
+// Enforce YARPP setup:
+$yarpp->enforce();
 
 // check to see that templates are in the right place
 if ( !count($yarpp->admin->get_templates()) ) {
@@ -62,7 +59,7 @@ if ( !yarpp_get_option('myisam_override') ) {
 		."'></input></form>"
 		."</div>";
 
-		$weight = yarpp_set_option('weight');
+		$weight = yarpp_get_option('weight');
 		unset($weight['title']);
 		unset($weight['body']);
 		yarpp_set_option(array('weight' => $weight));
@@ -123,6 +120,8 @@ if (isset($_POST['update_yarpp'])) {
 
 	if ( isset($_POST['exclude']) )
 		$new_options['exclude'] = implode(',',array_keys($_POST['exclude']));
+	else
+		$new_options['exclude'] = '';
 	
 	$new_options['template'] = isset($_POST['use_template']) ? $_POST['template_file'] : false;
 	$new_options['rss_template'] = isset($_POST['rss_use_template']) ? $_POST['rss_template_file'] : false;
