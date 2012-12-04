f231d3618af694e244aa7a6ca52d13346208ee97
mitchoyoshitaka <mitchoyoshitaka@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-04 17:01:26 +0000

    fix widget options
    git-svn-id: https://plugins.svn.wordpress.org/yet-another-related-posts-plugin/trunk@634092 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class-widget.php
+++ class-widget.php
@@ -78,41 +78,43 @@ class YARPP_Widget extends WP_Widget {
 		if ( isset($instance['use_template']) )
 			$instance['template'] = $instance['template_file'];
 	
+		$choice = false === $instance['template'] ? 'builtin' :
+			( $instance['template'] == 'thumbnails' ? 'thumbnails' : 'custom' );
+
 		// if there are YARPP templates installed...
 		$templates = $yarpp->admin->get_templates();
-		if ( count($templates) ): 
-			$choice = false === $instance['template'] ? 'builtin' :
-				( $instance['template'] == 'thumbnails' ? 'thumbnails' : 'custom' );
-			?>
+		if ( !count($templates) && $choice == 'custom' )
+			$choice = 'builtin';
+		
+		?>
 
-			<p>
-				<label style="padding-right: 10px;" for="<?php echo $this->get_field_id('use_template_builtin'); ?>"><input id="<?php echo $this->get_field_id('use_template_builtin'); ?>" name="<?php echo $this->get_field_name('use_template'); ?>" type="radio" value="builtin" <?php checked( $choice == 'builtin' ) ?> /> <?php _e( "List", 'yarpp' ); ?></label>
+		<p>
+			<label style="padding-right: 10px;" for="<?php echo $this->get_field_id('use_template_builtin'); ?>"><input id="<?php echo $this->get_field_id('use_template_builtin'); ?>" name="<?php echo $this->get_field_name('use_template'); ?>" type="radio" value="builtin" <?php checked( $choice == 'builtin' ) ?> /> <?php _e( "List", 'yarpp' ); ?></label>
+		
+			<label style="padding-right: 10px;" for="<?php echo $this->get_field_id('use_template_thumbnails'); ?>"><input id="<?php echo $this->get_field_id('use_template_thumbnails'); ?>" name="<?php echo $this->get_field_name('use_template'); ?>" type="radio" value="thumbnails" <?php checked( $choice == 'thumbnails' ) ?> /> <?php _e( "Thumbnails", 'yarpp' ); ?></label>
 			
-				<label style="padding-right: 10px;" for="<?php echo $this->get_field_id('use_template_thumbnails'); ?>"><input id="<?php echo $this->get_field_id('use_template_thumbnails'); ?>" name="<?php echo $this->get_field_name('use_template'); ?>" type="radio" value="thumbnails" <?php checked( $choice == 'thumbnails' ) ?> /> <?php _e( "Thumbnails", 'yarpp' ); ?></label>
-				
-				<label style="padding-right: 10px;" for="<?php echo $this->get_field_id('use_template_custom'); ?>"><input id="<?php echo $this->get_field_id('use_template_custom'); ?>" name="<?php echo $this->get_field_name('use_template'); ?>" type="radio" value="custom" <?php checked( $choice == 'custom' ) ?> /> <?php _e( "Custom", 'yarpp' ); ?></label>
-			</p>
+			<label style="padding-right: 10px;" for="<?php echo $this->get_field_id('use_template_custom'); ?>"><input id="<?php echo $this->get_field_id('use_template_custom'); ?>" name="<?php echo $this->get_field_name('use_template'); ?>" type="radio" value="custom" <?php checked( $choice == 'custom' ); disabled( !count($templates) ); ?> /> <?php _e( "Custom", 'yarpp' ); ?></label>
+		</p>
 
-			<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($instance['title']); ?>" /></label></p>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($instance['title']); ?>" /></label></p>
 
-			<p><label for="<?php echo $this->get_field_id('template_file'); ?>"><?php _e("Template file:",'yarpp');?></label> <select name="<?php echo $this->get_field_name('template_file'); ?>" id="<?php echo $this->get_field_id('template_file'); ?>">
-				<?php foreach ($templates as $template): ?>
-				<option value='<?php echo esc_attr($template['basename']); ?>'<?php selected($template['basename'], $instance['template']);?>><?php echo esc_html($template['name']); ?></option>
-				<?php endforeach; ?>
-			</select><p>
-			<script type="text/javascript">
-			jQuery(function($) {
-				function ensureTemplateChoice() {
-					var custom = $('#<?php echo $this->get_field_id('use_template_custom'); ?>').prop('checked');
-					var builtin = $('#<?php echo $this->get_field_id('use_template_builtin'); ?>').prop('checked');
-					$('#<?php echo $this->get_field_id('title'); ?>').closest('p').toggle(!!builtin);
-					$('#<?php echo $this->get_field_id('template_file'); ?>').closest('p').toggle(!!custom);
-				}
-				$('input[name="<?php echo $this->get_field_name('use_template'); ?>"]').change(ensureTemplateChoice);
-				ensureTemplateChoice();
-			});
-			</script>
-	<?php endif; ?>
+		<p><label for="<?php echo $this->get_field_id('template_file'); ?>"><?php _e("Template file:",'yarpp');?></label> <select name="<?php echo $this->get_field_name('template_file'); ?>" id="<?php echo $this->get_field_id('template_file'); ?>">
+			<?php foreach ($templates as $template): ?>
+			<option value='<?php echo esc_attr($template['basename']); ?>'<?php selected($template['basename'], $instance['template']);?>><?php echo esc_html($template['name']); ?></option>
+			<?php endforeach; ?>
+		</select><p>
+		<script type="text/javascript">
+		jQuery(function($) {
+			function ensureTemplateChoice() {
+				var custom = $('#<?php echo $this->get_field_id('use_template_custom'); ?>').prop('checked');
+				var builtin = $('#<?php echo $this->get_field_id('use_template_builtin'); ?>').prop('checked');
+				$('#<?php echo $this->get_field_id('title'); ?>').closest('p').toggle(!!builtin);
+				$('#<?php echo $this->get_field_id('template_file'); ?>').closest('p').toggle(!!custom);
+			}
+			$('input[name="<?php echo $this->get_field_name('use_template'); ?>"]').change(ensureTemplateChoice);
+			ensureTemplateChoice();
+		});
+		</script>
 
 		<p><input class="checkbox" id="<?php echo $this->get_field_id('promote_yarpp'); ?>" name="<?php echo $this->get_field_name('promote_yarpp'); ?>" type="checkbox" <?php checked($instance['promote_yarpp']) ?> /> <label for="<?php echo $this->get_field_id('promote_yarpp'); ?>"><?php _e("Help promote Yet Another Related Posts Plugin?",'yarpp'); ?></label></p>
 		<?php
