b1b413b70537c5e7f897609e0e964fa1c5836bff
Mike Nelson <michael@eventespresso.com>
2019-04-01 08:39:50 -0700

    namespace the_score and get_the_score, but also define the old versions if they dont conflict

--- includes/template_functions.php
+++ includes/template_functions.php
@@ -2,13 +2,31 @@
 
 // two YARPP-specific Template Tags, to be used in the YARPP-template Loop.
 
-function the_score() {
+function narpp_the_score() {
 	echo get_the_score();
 }
 
-function get_the_score() {
+function narpp_get_the_score() {
 	global $post;
 
 	$score = (float) $post->score;
 	return apply_filters('get_the_score', $score);
+}
+
+
+/**
+ * Only define `the_score` (and the other non-namespaced functions) for backward-compatibility with code that's
+ * already using them, and if they won't conflict with other plugins. Remember this gets loaded on 'init' priority 10
+ * so if another plugin defines them, they probably have already done so.
+ */
+if (! function_exists('the_score')){
+    function the_score(){
+        narpp_the_score();
+    }
+}
+
+if (! function_exists('get_the_score')){
+    function get_the_score(){
+        return narpp_get_the_score();
+    }
 }
